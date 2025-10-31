9e41e3b8abdca50e21e1e2f50ba152a753e1b0da
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-10-31 15:38:00 +0600

    Add solution for 398-find-the-difference problem
    Implements two methods to solve the LeetCode 'Find the Difference' problem using XOR and map-based approaches. Includes main function and utility macros for competitive programming.

---
+++ LeetCode/Back Again/398-find-the-difference.cpp
@@ -0,0 +1,104 @@
+//        ** Sumonta Saha Mridul **                                    SWE - SUST
+/*
+
+ *        ######    ##     ##  ##     ##   #######   ##    ##   ########      ###
+ !       ##    ##   ##     ##  ###   ###  ##     ##  ###   ##      ##        ## ##
+ ?       ##         ##     ##  #### ####  ##     ##  ####  ##      ##       ##   ##
+ *        ######    ##     ##  ## ### ##  ##     ##  ## ## ##      ##      ##     ##
+ !             ##   ##     ##  ##     ##  ##     ##  ##  ####      ##      #########
+ ?       ##    ##   ##     ##  ##     ##  ##     ##  ##   ###      ##      ##     ##
+ *        ######     #######   ##     ##   #######   ##    ##      ##      ##     ##
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define ll long long
+
+#define endl '\n'
+#define sp " "
+#define enter cout << endl;
+
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+#define INF numeric_limits<ll>::max();  //! 9223372036854775807
+#define NINF numeric_limits<ll>::min(); //! --9223372036854775808
+const int N = int(1e5 + 3);
+
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define min3(a, b, c) min(min((a), (b)), (c))
+
+//* 5 5 5 6 6 6 7 7
+//! lower = lower_bound(v.begin(), v.end(), 6) - v.begin();  ----- Ans : index - 3
+//! upper = upper_bound(v.begin(), v.end(), 6) - v.begin();  ----- Ans : index - 6
+//* char a = 'A';   int num = (int) a;
+//* char a = '2';   int num = a-48;
+//* char a = '8';   int num = a - '0' ;  (  s[i]-'0' == 8 )
+//! char a1 = 'a' + a - 1; 
+//?  vector<int> arr(n);
+//?  map<int, int> count; count[arr[i]]++;
+//?  pair<long, long>;  make_pair(a, b);
+//?  sort(arr.begin(), arr.end());
+
+void solve()
+{
+  //* Sometimes you win, sometimes you learn..." - Good Wisher
+
+}
+
+char findTheDifference(string s, string t) {
+  char answer  = '\0';
+  for (auto value : s) answer ^= value;
+  for (auto value : t) answer ^= value;
+
+  return answer;
+}
+
+char findTheDifference(string s, string t) {
+
+  int sizeS = s.size();
+  int sizeT = t.size();
+
+  char addedLetter = 's';
+  map<char, int> baseStore;
+
+  for (int i = 0; i < sizeS; i++) {
+      char temp = t[i];
+      baseStore[temp]++;
+  }
+
+  for (int i = 0; i < sizeT; i++) {
+      char temp = s[i];
+      baseStore[temp]--;
+  }
+
+  for (auto value : baseStore) {
+      if (value.second == 1) {
+          addedLetter = value.first;
+          break;
+      }
+  }
+
+  return addedLetter;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve(); //* "Sumonta was heree....."
+    }
+    return 0;
+}
